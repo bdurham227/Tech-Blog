dc068a43d0ffa298bb5ab42773692a612d524e49
bdurham227 <bdurham227@gmail.com>
2021-05-14 14:49:59 -0400

    built out get post by id route

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -28,7 +28,47 @@ router.get('/', async (req, res) => {
   } catch (err) {
     res.status(500).json(err)
   }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+
+    const postData = await Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ["id", "post_title", "post_content", "date_created"],
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          attributes: ["id", "post_id", "user_id", "comment_body"],
+        },
+      ],
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: `No Post with the ID ${req.params.id} found `});
+      return;
+    }
+
+    const post = postData.get({ plain: true });
+    res.status(200).json(post);
+
+
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
 })
+
+
+
+
+
 
 
 
